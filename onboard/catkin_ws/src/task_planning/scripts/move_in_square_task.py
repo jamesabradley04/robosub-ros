5eae970f69b04dcb8b8c5187e54f4c6a6ead2e84
echang264 <eric.t.chang@duke.edu>
2021-05-04 11:18:48 -0400

    change move-in-square-task to use ListTask

--- onboard/catkin_ws/src/task_planning/scripts/move_in_square_task.py
+++ onboard/catkin_ws/src/task_planning/scripts/move_in_square_task.py
@@ -1,11 +1,13 @@
 from task import Task
-from move_tasks import MoveToPoseLocalTask
+from move_tasks import MoveToPoseLocalTask, MoveToPoseGlobalTask
+from combination_tasks import ListTask
 import task_utils
+
 
 class MoveInSquareTask(Task):
     """Move the robot in a square at a certain depth."""
 
-    def __init__(self, movement_distance, depth=0): # maybe replace this with a dict or list
+    def __init__(self, movement_distance, depth=0):  # maybe replace this with a dict or list
         """
         Parameters:
             move_distance(float): distance of each leg of the square path
@@ -13,19 +15,16 @@ class MoveInSquareTask(Task):
         """
         super(MoveInSquareTask, self).__init__()
 
-        self.step_one = MoveToPoseLocalTask(movement_distance, 0, depth, 0, 0, 0)
-        self.step_two = MoveToPoseLocalTask(0, movement_distance, depth, 0, 0, 0)
-        self.step_three = MoveToPoseLocalTask(-movement_distance, 0, depth, 0, 0, 0)
-        self.step_four = MoveToPoseLocalTask(0, -movement_distance, depth, 0, 0, 0)
+        self.step_one = MoveToPoseGlobalTask(movement_distance, 0, depth, 0, 0, 0)
+        self.step_two = MoveToPoseGlobalTask(0, movement_distance, depth, 0, 0, 0)
+        self.step_three = MoveToPoseGlobalTask(-movement_distance, 0, depth, 0, 0, 0)
+        self.step_four = MoveToPoseGlobalTask(0, -movement_distance, depth, 0, 0, 0)
+        self.list_task = ListTask([self.step_one, self.step_two, self.step_three, self.step_four])
 
-    
     def _on_task_start(self):
         pass
-    
-    def _on_task_run(self):
-        self.step_one.run()
-        self.step_two.run()
-        self.step_three.run()
-        self.step_four.run()
 
-        self.finish()
+    def _on_task_run(self):
+        self.list_task.run()
+        if(self.list_task.finished):
+            self.finish()
