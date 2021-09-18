9cf8c575e3e1dab521f63ceaa1f30b8a786333a4
Brokemia <rrwthird@aol.com>
2021-09-18 14:07:08 -0400

    Change to move in square with smach

--- onboard/catkin_ws/src/task_planning/scripts/smach_test.py
+++ onboard/catkin_ws/src/task_planning/scripts/smach_test.py
@@ -16,8 +16,17 @@ def main():
     # Open the container
     with sm:
         # Add states to the container
-        smach.StateMachine.add('Move', MoveToPoseGlobalTask(2, 0, 0, 0, 0, 0), 
-                               transitions={'spin':'Move',
+        smach.StateMachine.add('Move1', MoveToPoseGlobalTask(2, 0, 0, 0, 0, 0), 
+                               transitions={'spin':'Move1',
+                                            'done':'Move2'})
+        smach.StateMachine.add('Move2', MoveToPoseGlobalTask(2, 2, 0, 0, 0, 0), 
+                               transitions={'spin':'Move2',
+                                            'done':'Move3'})
+        smach.StateMachine.add('Move3', MoveToPoseGlobalTask(0, 2, 0, 0, 0, 0), 
+                               transitions={'spin':'Move3',
+                                            'done':'Move4'})
+        smach.StateMachine.add('Move4', MoveToPoseGlobalTask(0, 0, 0, 0, 0, 0), 
+                               transitions={'spin':'Move4',
                                             'done':'finish'})
 
     # Execute SMACH plan
