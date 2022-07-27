54d131ccece47734e890d3ac941a05c2c066d3b9
tylerfeldman321 <tylerfeldman321@gmail.com>
2022-07-27 13:48:28 -0400

    Create function for connecting to device and setting up output queues

--- onboard/catkin_ws/src/cv/scripts/spatial_detection.py
+++ onboard/catkin_ws/src/cv/scripts/spatial_detection.py
@@ -14,6 +14,7 @@ class DepthAISpatialDetector:
             raise FileNotFoundError(f'Required file/s not found, please run "{sys.executable} install_requirements.py"')
 
         self.pipeline = self.get_pipeline(nnBlobPath)
+        self.output_queues = {}
 
     def get_pipeline(self, nnBlobPath, syncNN=True):
         pipeline = dai.Pipeline()
@@ -81,61 +82,58 @@ class DepthAISpatialDetector:
 
         return pipeline
 
-    def run_detection(self):
+    def connect_and_get_output_queues(self):
         # Connect to device and start pipeline
         with dai.Device(self.pipeline) as device:
+            self.output_queues["previewQueue"] = device.getOutputQueue(name="rgb", maxSize=1, blocking=False)
+            self.output_queues["detectionNNQueue"] = device.getOutputQueue(name="detections", maxSize=1, blocking=False)
+            self.output_queues["xoutBoundingBoxDepthMappingQueue"] = device.getOutputQueue(name="boundingBoxDepthMapping", maxSize=1, blocking=False)
+            self.output_queues["depthQueue"] = device.getOutputQueue(name="depth", maxSize=1, blocking=False)
 
-            # Output queues will be used to get the rgb frames and nn data from the outputs defined above
-            previewQueue = device.getOutputQueue(name="rgb", maxSize=1, blocking=False)
-            detectionNNQueue = device.getOutputQueue(name="detections", maxSize=1, blocking=False)
-            xoutBoundingBoxDepthMappingQueue = device.getOutputQueue(name="boundingBoxDepthMapping", maxSize=1, blocking=False)
-            depthQueue = device.getOutputQueue(name="depth", maxSize=1, blocking=False)
+    def run_detection(self):
+        inPreview = self.output_queues["previewQueue"].get()
+        inDet = self.output_queues["detectionNNQueue"].get()
+        depth = self.output_queues["depthQueue"].get()
 
-            inPreview = previewQueue.get()
-            inDet = detectionNNQueue.get()
-            depth = depthQueue.get()
+        frame = inPreview.getCvFrame()
+        detections = inDet.detections
 
-            frame = inPreview.getCvFrame()
-            depthFrame = depth.getFrame()  # depthFrame values are in millimeters
-
-            depthFrameColor = cv2.normalize(depthFrame, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
-            depthFrameColor = cv2.equalizeHist(depthFrameColor)
-            depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
-
-            detections = inDet.detections
-
-            # if len(detections) != 0:
-            #     boundingBoxMapping = xoutBoundingBoxDepthMappingQueue.get()
-            #     roiDatas = boundingBoxMapping.getConfigData()
-            #
-            #     for roiData in roiDatas:
-            #         roi = roiData.roi
-            #         roi = roi.denormalize(depthFrameColor.shape[1], depthFrameColor.shape[0])
-            #         topLeft = roi.topLeft()
-            #         bottomRight = roi.bottomRight()
-            #         xmin = int(topLeft.x)
-            #         ymin = int(topLeft.y)
-            #         xmax = int(bottomRight.x)
-            #         ymax = int(bottomRight.y)
+        # depthFrame = depth.getFrame()  # depthFrame values are in millimeters
+        # depthFrameColor = cv2.normalize(depthFrame, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
+        # depthFrameColor = cv2.equalizeHist(depthFrameColor)
+        # depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
+        # if len(detections) != 0:
+        #     boundingBoxMapping = xoutBoundingBoxDepthMappingQueue.get()
+        #     roiDatas = boundingBoxMapping.getConfigData()
+        #
+        #     for roiData in roiDatas:
+        #         roi = roiData.roi
+        #         roi = roi.denormalize(depthFrameColor.shape[1], depthFrameColor.shape[0])
+        #         topLeft = roi.topLeft()
+        #         bottomRight = roi.bottomRight()
+        #         xmin = int(topLeft.x)
+        #         ymin = int(topLeft.y)
+        #         xmax = int(bottomRight.x)
+        #         ymax = int(bottomRight.y)
 
             # If the frame is available, draw bounding boxes on it and show the frame
-            height = frame.shape[0]
-            width = frame.shape[1]
-            for detection in detections:
-                # Denormalize bounding box
-                x1 = int(detection.xmin * width)
-                x2 = int(detection.xmax * width)
-                y1 = int(detection.ymin * height)
-                y2 = int(detection.ymax * height)
+        height = frame.shape[0]
+        width = frame.shape[1]
+        for detection in detections:
+            # Denormalize bounding box
+            x1 = int(detection.xmin * width)
+            x2 = int(detection.xmax * width)
+            y1 = int(detection.ymin * height)
+            y2 = int(detection.ymax * height)
 
-                label = detection.label
+            label = detection.label
 
-                confidence = detection.confidence
-                x = detection.spatialCoordinates.x
-                y = detection.spatialCoordinates.y
-                z = detection.spatialCoordinates.z
+            confidence = detection.confidence
+            x = detection.spatialCoordinates.x
+            y = detection.spatialCoordinates.y
+            z = detection.spatialCoordinates.z
 
-                print(f'Label: {label}, Confidence: {confidence}, X: {x}, Y: {y}, Z: {z}')
+            print(f'Label: {label}, Confidence: {confidence}, X: {x}, Y: {y}, Z: {z}')
 
 
 if __name__ == '__main__':
