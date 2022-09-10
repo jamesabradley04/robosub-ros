8d276b649e27ec9a889cf4a228502cc66f6a9ed0
tyler.feldman <tyler.feldman@duke.edu>
2022-09-10 14:19:04 -0400

    Refactor footage extraction code into class

--- footage_extraction/extract_footage.py
+++ footage_extraction/extract_footage.py
@@ -4,48 +4,76 @@ import cv2
 import image_tools
 import os
 
-FILE_NAME = "gate_and_buoy_footage_3"
-FILE_EXTENSION = ".bag"
-FOLDER_PATH = '/root/dev/robosub-ros/footage_extraction/footage/' # with / at end
-FOOTAGE_PATH = FOLDER_PATH + FILE_NAME + FILE_EXTENSION
-FRAMES_PATH = FOLDER_PATH + FILE_NAME + '/'
-STEP_SIZE = 10
-TOPIC_NAME = "/camera/front/stream_raw"
 
-print("Do you want to view rosbag info? (y/n): ", end="")
-view_rosbag_info = input()
+# Extracts footage from rosbag files
+class ROSBagFootageExtractor:
 
-if view_rosbag_info == "y":
-    # terminal command for rosbag info
-    os.system("rosbag info " + FOOTAGE_PATH)
-    print()
+    def view_rosbag_info(self, footage_path):
+        """
+        Call rosbag info on the rosbag file. This provides a human-readable summary of the file contents, including
+        what topic was recorded.
 
-print("Do you want to continue with extracting frames from the rosbag? (y/n): ", end="")
-extract_rosbag_frames = input()
-if extract_rosbag_frames == "y":
-    bag = rosbag.Bag(FOOTAGE_PATH)
-    bridge = CvBridge()
-    frame_count = 0
-    num_frames_saved = 0
-    image_tools = image_tools.ImageTools()
+        :param footage_path: Path to rosbag file.
+        :return: Prints summary of the file contents.
+        """
+        os.system("rosbag info " + footage_path)
+        print()
 
-    save_dir = os.path.join(FOLDER_PATH, FILE_NAME)
-    if not os.path.exists(save_dir):
-        os.mkdir(save_dir)
+    def extract_frames(self, footage_path, topic_name, output_dir, step_size=10):
+        """
+        Extract frames from the rosbag file and saves them into self.output_dir
 
-    for topic, msg, t in bag.read_messages(topics=[TOPIC_NAME]):
-        if topic == TOPIC_NAME:
-            if frame_count % STEP_SIZE == 0:
-                cv_img = image_tools.convert_ros_msg_to_cv2(msg)
-                file_save_path = os.path.join(save_dir, f"{FILE_NAME}_{frame_count}.jpg")
-                success = cv2.imwrite(file_save_path, cv_img)
-                print(f"File path: {file_save_path}, success: {success}")
-                if success:
-                    num_frames_saved += 1
-            frame_count += 1
+        :param footage_path: Path to rosbag file.
+        :param topic_name: Name of the topic where the image feed is published. Can find by calling view_rosbag_info
+        on the rosbag file.
+        :param output_dir: Path for directory to save the output. Will create a subdirectory within output_dir for each
+        file that is extracted.
+        :param step_size: Will extract a frame every step_size number of frames.
+        :return: Will
+        """
 
-    print(f"Read {frame_count} frames")
-    print(f"Saved {num_frames_saved} frames to {FRAMES_PATH + FILE_NAME}")
+        file_basename = os.path.basename(footage_path)
+        file_name_no_extension = os.path.splitext(file_basename)[0]
 
-    # Close the bagfile
-    bag.close()
+        bag = rosbag.Bag(footage_path)
+
+        frame_count = 0
+        num_frames_saved = 0
+        image_tools = image_tools.ImageTools()
+
+        save_dir = os.path.join(output_dir, file_name_no_extension)
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+
+        for topic, msg, t in bag.read_messages(topics=[topic_name]):
+            if topic == topic_name:
+                if frame_count % step_size == 0:
+
+                    cv_img = image_tools.convert_ros_msg_to_cv2(msg)
+
+                    file_save_path = os.path.join(save_dir, f"{file_name_no_extension}_{frame_count}.jpg")
+                    success = cv2.imwrite(file_save_path, cv_img)
+
+                    print(f"File path: {file_save_path}, success: {success}")
+
+                    if success:
+                        num_frames_saved += 1
+
+                frame_count += 1
+
+        print(f"Read {frame_count} frames")
+        print(f"Saved {num_frames_saved} frames to {output_dir + file_name}")
+
+        bag.close()
+
+if __name__ == '__main__':
+
+    FOOTAGE_PATH = '/root/dev/robosub-ros/footage_extraction/footage/test.bag'
+    OUTPUT_DIR = '/root/dev/robosub-ros/footage_extraction/footage/'
+    TOPIC_NAME = "/camera/front/stream_raw"
+
+    footage_extractor = ROSBagFootageExtractor()
+
+    footage_extractor.view_rosbag_info(FOOTAGE_PATH)
+
+    footage_extractor.extract_frames(FOOTAGE_PATH, TOPIC_NAME, OUTPUT_DIR)
